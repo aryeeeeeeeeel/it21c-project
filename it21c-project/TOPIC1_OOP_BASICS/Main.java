d3ae0e3f39f6c75a22147e906756ed671df728bb
aryeeeeeeeeel <arielsumantin69@gmail.com>
2023-11-17 13:35:14 +0800

    Added Main.java and Vegetables.java

---
+++ it21c-project/TOPIC1_OOP_BASICS/Main.java
@@ -0,0 +1,12 @@
+package TOPIC1_OOP_BASICS;
+
+public class Main {
+    
+     public static void main(String[] args) {
+        Vegetables Vegetables_01 = new Vegetables ("Utan ni", 342.98);
+        
+        Vegetables_01.showVegetablesName();
+        Vegetables_01.showVegetablesPrice();
+    }
+    
+}
